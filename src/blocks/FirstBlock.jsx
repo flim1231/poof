7f3dcea00a5474a3214cde07e252e1a886b06df8
flim1231 <pudenkovsasha25@gmail.com>
2022-05-31 15:49:51 +0300

    first commit

---
+++ src/blocks/FirstBlock.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import clas from './style.module.css'
+
+function FirstBlock() {
+    return ( 
+        <div>
+
+        </div>
+     );
+}
+
+export default FirstBlock;
