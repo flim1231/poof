45996adea4aaac83e6f6134b39408e8054e6b352
YaroslavRat <ratusnika@gmail.com>
2022-06-06 01:49:18 +0300

    choose a tour slider start

--- src/blocks/CustomSlider.jsx
+++ src/blocks/CustomSlider.jsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 import { Carousel } from 'react-bootstrap'
 import './styleSlide.css'
 
-import slide1 from './../imgs/firstblock/slide1.png'
-import slide2 from './../imgs/firstblock/slide2.png'
-import slide3 from './../imgs/firstblock/slide3.png'
-import slide4 from './../imgs/firstblock/slide4.png'
-import slide5 from './../imgs/firstblock/slide5.png'
+import img1shadow from './../imgs/firstblock/img1shadow.png'
+import img2shadow from './../imgs/firstblock/img2shadow.png'
+import img3shadow from './../imgs/firstblock/img3shadow.png'
+import img4shadow from './../imgs/firstblock/img4shadow.png'
+import img5shadow from './../imgs/firstblock/img5shadow.png'
 
 class CustomSlider extends Component {
     constructor(props) {
@@ -17,66 +17,89 @@ class CustomSlider extends Component {
 
         return (
             <div className='head1'>
-
+                
                 <Carousel nextIcon={false} prevIcon={false}>
                 <Carousel.Item interval={4000}>
                     <img
                         className="d-block w-100"
-                        src={slide1}
+                        src={img1shadow}
                         alt="First slide"
                     />
+                    <Carousel.Caption>
+                        <h3 className="slider_title">Iceland</h3>
+                        <p className="slider_subtitle">Experience the splendor of nature</p>
+                    </Carousel.Caption>
                 </Carousel.Item>
-
+                
                 <Carousel.Item interval={4000}>
                     <img
                         className="d-block w-100"
-                        src={slide2}
+                        src={img2shadow}
                         alt="Second slide"
                     />
                     <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <h3 className="slider_title">Iceland</h3>
+                        <p className="slider_subtitle">Experience the splendor of nature</p>
                     </Carousel.Caption>
                 </Carousel.Item>
 
                 <Carousel.Item interval={4000}>
                     <img
                         className="d-block w-100"
-                        src={slide3}
+                        src={img3shadow}
                         alt="Third slide"
                     />
                     <Carousel.Caption interval={4000}>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        <h3 className="slider_title">Iceland</h3>
+                        <p className="slider_subtitle">Experience the splendor of nature</p>
                     </Carousel.Caption>
                 </Carousel.Item>
 
                 <Carousel.Item interval={4000}>
                     <img
                         className="d-block w-100"
-                        src={slide4}
-                        alt="Third slide"
+                        src={img4shadow}
+                        alt="fourth slide"
                     />
                     <Carousel.Caption interval={4000}>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        <h3 className="slider_title">Iceland</h3>
+                        <p className="slider_subtitle">Experience the splendor of nature</p>
                     </Carousel.Caption>
                 </Carousel.Item>
 
                 <Carousel.Item interval={4000}>
                     <img
                         className="d-block w-100"
-                        src={slide5}
-                        alt="Third slide"
+                        src={img5shadow}
+                        alt="fifth slide"
                     />
                     <Carousel.Caption interval={4000}>
-                        <h3>Third slide label</h3>
-                        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        <h3 className="slider_title">Iceland</h3>
+                        <p className="slider_subtitle">Experience the splendor of nature</p>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
+                {/* Header menu */}
+                <div className="header_menu">
+                        <nav className="menu_body">
+                            <ul className="menu_list">
+                                <li className="list_item1"><a href="#">Route</a></li>
+                                <li className="list_item2"><a href="#">Dates</a></li>
+                                <li className="list_item3"><a href="#">About us</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                {/* Header logo */}
+                    <div className="header_logo">
+                        <span className="logo_text">Smart Travel</span>
+                    </div>
+                {/* Header number */}
+                    <div className="header_number">
+                        <span className="number_text">+375 (28) 829 49 24 </span>
+                    </div>
             </div>
-            
+                
+
         );
     }
 
--- src/blocks/TwoBlock.jsx
+++ src/blocks/TwoBlock.jsx
@@ -3,8 +3,71 @@ import cl from './TwoBlock.module.css'
 
 function FirstBlock() {
     return ( 
-        <div>
-            
+        <div className={cl.wrapper}>
+
+
+            {/* Iceland info */}
+
+            <div className={cl.iceland_info}>
+
+                {/* Iceland_title_title */}
+                <span className={cl.iceland_title_title}>Iceland</span>
+                {/* Iceland title */}
+
+                <div className={cl.iceland_title}>
+
+                    A land of huge volcanoes and <br />
+                    protected green fields dotted with  <br />
+                    tranquility and serenity. "Land of  <br />
+                    Ice" is a country that welcomes all  <br />
+                    travelers to its land <br />
+                </div>
+
+                {/* Iceland subtitle */}
+
+                <div className={cl.iceland_subtitle}>
+                    Mountains, rocky cliffs, undulating and smooth like waves in the <br />
+                    Atlantic Ocean mountaineering enthusiasts, and just people who want <br />
+                    a break from vanity should visit Iceland. 
+                </div>
+
+                {/* Iceland button */}
+
+                <div className={cl.iceland_button}>
+                    <a href="#" className={cl.iceland_button_text}>Read on</a>
+                </div>
+
+                {/* Iceland mountains */}
+
+                <div className={cl.iceland_mountains}>
+                    <span className={cl.mountains_img}></span>
+                </div>
+
+                {/* Iceland waterfall */}
+
+                <div className={cl.iceland_waterfall}>
+                    <span className={cl.waterfall_img}></span>
+                </div>
+
+            </div>
+
+            <div className={cl.choose_tour}>
+
+                <span className={cl.choose_tour_text}>Choose a tour</span>
+
+                <span className={cl.filter1}>With good reviews</span>
+
+                <div className={cl.filter1_cancel}></div>
+
+                <span className={cl.filter2}>Popular Tours</span>
+
+                <div className={cl.filter2_cancel}></div>
+
+                <span className={cl.filter3}>From cheap to expensive</span>
+
+                <div className={cl.filter3_cancel}></div>
+
+            </div>
         </div>
      );
 }
